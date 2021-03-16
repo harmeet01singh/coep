ccd8cbc443e8d1c5bad29bc5118db3115406741d
harmeetsingh01 <2018.harmeet.kathoda@ves.ac.in>
2021-03-16 19:31:39 +0530

    delete master root oldRepo files

--- coepLatex/latex_module/latex.py
+++
@@ -1,8 +0,0 @@
-def superscript(variable, power):
-    return f'{variable}^{{{power}}}'
-
-def convert_to_term(coefficient, variable1, power1, variable2, power2):
-    return (str(coefficient) if coefficient != 1 else '') + superscript(variable1, power1) + superscript(variable2, power2)
-
-def to_frac(numerator, denominator):
-    return f'$\\frac{{{numerator}}}{{{denominator}}}$'
--- linear equation in one variable/abc.py
+++
@@ -1,8 +0,0 @@
-import os
-import git
-
-print(os.getcwd())
-repo = git.Repo('.', search_parent_directories=True)
-print(repo.working_tree_dir)
-print(__file__)
-print(os.path.basename(__file__).split('.')[0])
