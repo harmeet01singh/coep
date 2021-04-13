33739025ec8456c887313e64773e95a9f41fdc2b
RohanpolGIT <rohanpol36@gmail.com>
2021-04-13 16:47:19 +0530

    030102,030201| v8_1,v3_1 | RohanpolGIT | 13/4/21

--- 0301/030102/v8_1.py
+++ 0301/030102/v8_1.py
@@ -4,7 +4,8 @@ import random as rd
 from coep_package.csv_module import database_fn,putInCsv
 from coep_package.latex import latex
 
-def generate_expression(key):
+def generate_expression():
+    key = rd.randint(0,1)
     #return equal expressions if key == 1 or unequal expressions if key == 0
     if (key):
         exp1,exp2,exp3 = '0','1','3'
@@ -23,27 +24,18 @@ def generate_expression(key):
             exp3 = '{} * {}'.format(rd.randint(1,10),rd.randint(1,10))
         #print(eval(exp1),eval(exp2),eval(exp3))
         key = False
-    exps = [exp1,exp2,exp3]
-    rd.shuffle(exps)
-    return exps[0:2]
+    return [exp1,exp2,exp3,key]
 
-def print_questions():
+def print_questions(exp=[]):
+    rd.shuffle(exp)
     #print(exp)
-    question = '\n <br> Which of the following pairs will not form a balanced equation ?'
+    question = '\n <br> {} and {}, do they represent two sides of an equation ?'.format(latex(exp[0]),latex(exp[1]))
     print(question)
-    return question
+    return exp,question
 
-def format_options(exp):
-    return latex(exp[0]+","+exp[1])
-
-def print_options(exp):
-    op1 = format_options(exp)
-    op2 = format_options(generate_expression(1))
-    op3 = format_options(generate_expression(1))
-    op4 = format_options(generate_expression(1))
-    option_str = f'''\n1) {op1} \n2) {op2} \n3) {op3} \n4) {op4}'''
-    print(option_str)
-    return [op1,op2,op3,op4]
+def print_options():
+    print('''\n1) True
+             \n2) False''')
 
 def take_input(corr_op):
     ans = None
@@ -62,7 +54,8 @@ def take_input(corr_op):
 
 
 def print_solution(exp):
-    section1='''<br> \nTo check equality of expressions we will check if both of them give same result or not'''
+    section1='''<br>
+            \nTo check equality of expressions we will check if both of them give same result or not'''
     print(section1)
 
     expression1 = '{} = {}'.format(exp[0],eval(exp[0]))
@@ -73,45 +66,40 @@ def print_solution(exp):
     section3 =  '''\n <br>Second expression {}'''.format(latex(expression2))
     print(section3)
 
-    section4 = '''\n<br>Since both expressions give different values, they do not form a balanced equation'''
     if(eval(exp[0]) == eval(exp[1])):
         section4='''\n<br>Since both expression give same values i.e. {}, answer is True'''.format(latex(eval(exp[0])))
         print(section4)
     else:
-        
+        section4 = '''\n<br>Since both expressions give different values, the answer is False'''
         print(section4)
     solution = section1 + section2 + section3 + section4
     return solution
 
 
 def main():
-    e0,e1 = generate_expression(0)
-    exp=[e0,e1]
+    e0,e1,e2,corr_op = generate_expression()
     #print([e0,e1,e2])
-    Question = print_questions()
-    corr_op,wrong_op1,wrong_op2,wrong_op3 = print_options(exp)
+    exp,Question = print_questions([e0,e1,e2])
+    print_options()
     #take_input(not(corr_op))
     wrong_op = not(corr_op)
     Solution = print_solution(exp)
 
     database_dict= database_fn(
-        Answer_Type='1',
-        Topic_Number='030201',
-        Variation='v3_1',
+        Answer_Type='4',
+        Topic_Number='030102',
+        Variation='v8_1',
         Question=Question,
         Correct_Answer_1=corr_op,
-        Wrong_Answer_1=wrong_op1,
-        Wrong_Answer_2=wrong_op2,
-        Wrong_Answer_3=wrong_op3,
+        Wrong_Answer_1=wrong_op,
         Solution_text=Solution,
         ContributorMail="2018.rohan.pol@ves.ac.in"
     )
     return database_dict
 
 putInCsv(
-    Topic_Number='030201',
+    Topic_Number='030102',
     Number_Of_Iterations=10,
     Main_Function=main,
-    Filename=__file__,
-    Remove_Duplicates=False
+    Filename=__file__
 )
